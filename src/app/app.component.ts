7f067469217ae5579b7b2ec28f80eb0957b4db05
Alex Zeiser <alex.zeiser.rest@gmail.com>
2024-08-02 15:17:29 +0200

    create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { WhyMeSectionComponent } from "./why-me-section/why-me-section.component";
+import { MySkillsSectionComponent } from './my-skills-section/my-skills-section.component';
+import { MyProjectsSectionComponent } from './my-projects-section/my-projects-section.component';
+import { ContactMeSectionComponent } from './contact-me-section/contact-me-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MenuBarComponent,
+    WhyMeSectionComponent,
+    MySkillsSectionComponent,
+    MyProjectsSectionComponent,
+    ContactMeSectionComponent,
+    FooterComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { WhyMeSectionComponent } from './why-me-section/why-me-section.component';
+import { MySkillsSectionComponent } from './my-skills-section/my-skills-section.component';
+import { MyProjectsSectionComponent } from './my-projects-section/my-projects-section.component';
+import { ContactMeSectionComponent } from './contact-me-section/contact-me-section.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+
+
+
+export const routes: Routes = [
+    { path: 'menu-bar', component: MenuBarComponent },
+    { path: 'why-me-section', component: WhyMeSectionComponent },
+    { path: 'my-skills-section', component: MySkillsSectionComponent },
+    { path: 'mmy-projects-section', component: MyProjectsSectionComponent },
+    { path: 'contact-me-section', component: ContactMeSectionComponent },
+    { path: 'footer', component: FooterComponent }
+];
