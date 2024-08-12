0f2cef240ada67b3567f43fbd1cc0a2ec4b96f00
Alex Zeiser <alex.zeiser.rest@gmail.com>
2024-08-12 10:35:07 +0200

    version 1.0.0.2

--- src/app/contact-me-section/contact-me-section.component.ts
+++ src/app/contact-me-section/contact-me-section.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-me-section',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-me-section.component.html',
-  styleUrl: './contact-me-section.component.scss'
+  styleUrls: ['./contact-me-section.component.scss']
 })
 export class ContactMeSectionComponent {
+  constructor(private router: Router) { }
 
+  http = inject(HttpClient);
+
+  contactData = {
+    name: "",
+    email: "",
+    message: ""
+  }
+
+  mailTest = false;
+
+  post = {
+    endPoint: 'http://alex-zeiser-developer.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+            console.info('E-Mail erfolgreich gesendet');
+          },
+          error: (error) => {
+            console.error('Fehler beim Senden der E-Mail:', error);
+          },
+          complete: () => console.info('Sendevorgang abgeschlossen'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+      console.info('MailTest aktiv, keine E-Mail gesendet');
+    }
+  }
+  navigateToHeader(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
--- src/app/why-me-section/why-me-section.component.ts
+++ src/app/why-me-section/why-me-section.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-why-me-section',
   standalone: true,
-  imports: [],
   templateUrl: './why-me-section.component.html',
-  styleUrl: './why-me-section.component.scss'
+  styleUrls: ['./why-me-section.component.scss']
 })
 export class WhyMeSectionComponent {
+  constructor(private router: Router) { }
 
+  navigateToContactMe(): void {
+    this.router.navigate(['/contact-me-section'], { fragment: 'contact-me-container' }).then(() => {
+      const element = document.getElementById('contact-me-container');
+      if (element) {
+        const headerOffset = 100;
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.scrollY - headerOffset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: 'smooth'
+        });
+      }
+    });
+  }
 }
